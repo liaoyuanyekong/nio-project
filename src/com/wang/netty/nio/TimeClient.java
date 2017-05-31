108e9299e748a5e442646519ec3e402691410285
WangZhihao <javabeanwzh@163.com>
2017-05-31 17:44:18 +0800

    netty 客户端开发

---
+++ src/com/wang/netty/nio/TimeClient.java
@@ -0,0 +1,45 @@
+package com.wang.netty.nio;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 客户端代码
+ * Created by WangZhihao on 2017/5/31.
+ */
+public class TimeClient {
+
+    public void connect(int port, String host)throws Exception{
+        EventLoopGroup group =new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap=new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port=8080;
+        new TimeClient().connect(port,"127.0.0.1");
+    }
+
+
+
+
+
+
+}
--- src/com/wang/netty/nio/TimeServerHandler.java
+++ src/com/wang/netty/nio/TimeServerHandler.java
@@ -1,12 +1,42 @@
 package com.wang.netty.nio;
 
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
 
 /**
  * Created by WangZhihao on 2017/5/30.
  */
-public class TimeServerHandler extends ChannelHandlerAdapter{
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
 
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //读取数据
+        ByteBuf buf=(ByteBuf)msg;
+        byte[] req=new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body =new String(req,"UTF-8");
+        System.out.println("The time server receive order "+body);
+        String currentTime="QUERY TIME ORDER".equalsIgnoreCase(body)?
+                new Date(System.currentTimeMillis()).toString():"BAD ORDER";
+        //写入数据
+        ByteBuf resp= Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
 
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
 }
